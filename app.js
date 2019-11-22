914f9e5099dbacda5784f42cd36d3a0a4280f679
taroshuntaro <shoji.dev.shuntaro@gmail.com>
2019-11-22 12:09:47 +0900

    sample

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended:true }))
+app.use(bodyParser.json())
+
+app.listen(3000)
+
+console.log('start!')
+
+app.get('/api/get', function(req, res) {
+    const query = req.query
+    console.log(query)
+    res.json(query)
+})
